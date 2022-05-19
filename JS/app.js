1499ca5f1f6c8a85867b4c5339036248c00eb13a
jhonas12000 <ytwmichael@gmail.com>
2022-05-19 13:06:36 -0700

    Landing page project is done and working

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,5 @@
 console.log('javaScript connected properly');
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
 
 
 // Defining Global Variables
@@ -25,48 +7,66 @@ const navbarMenu = document.querySelector('.navbar_menu');
 const navbarList = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
 //Building the Navbar and link properly to their respective sections
-
 const buildNav = () => {
-    sections.forEach(section => {   
-        const newElement = document.createElement('li');
+    sections.forEach(section => { 
         const sectionId = section.id;
-        const sectionData = section.dataset.nav
+        const sectionData = section.dataset.nav 
+        //creating list  
+        const newElement = document.createElement('li');
+        //adding contents to the li list
         newElement.innerHTML = `<li><a class='menu__link' href='#${sectionId}'>${sectionData}</a></li>`;
+        //attaching the list elements to the ul 
         navbarList.appendChild(newElement);
         
     })
 }
 buildNav();
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
 
 // Add class 'active' to section when near top of viewport
+function activeClass (section) {
+    return Math.floor(section.getBoundingClientRect().top)
+};
 
+//removing the default active class 
+function removeActiveClass (section) {
+    
+    const element = document.getElementsByClassName('your-active-class');
+    for(let i = 0; i< element.length; i++)
+    //keep the non active class its default color
+    {
+        element[i].style.backgroundColor = "transparent";
+    }
+    section.classList.remove('your-active-class');  
+}
 
-// Scroll to anchor ID using scrollTO event
+//adding active class whenever the pointer is in the respective section
+function addActiveClass (active, section) {
+    if(active) {
+        section.classList.add('your-active-class')
+    }
+    
+    const element = document.getElementsByClassName('your-active-class');
+    //looping through all the sections
+        for(let i = 0; i< element.length; i++)
+        //change into the given color of the active class
+        {
+            element[i].style.backgroundColor = "gray";
+        }
+}
 
+//impelemen adding and removing function
+function implementation () {
+    sections.forEach(section => {
+        const viewContent = activeClass(section);
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+        viewSize = () => viewContent <= 300 && viewContent >= -300;
 
-// Build menu 
+        removeActiveClass(section);
+        addActiveClass(viewSize(), section);
+        
+    })
+};
+window.addEventListener('scroll', implementation);
 
-// Scroll to section on link click
-
-// Set sections as active
